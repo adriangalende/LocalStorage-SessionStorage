4cc55e7623414f2648cea1916a00c9f0764b6a5f
Adrián <adriangalende@gmail.com>
2019-03-02 11:57:51 +0100

    Funcional con localStorage

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 window.onload = function(){
     let agendas = []
+    var formulario = document.forms['form-add-agenda'];
+    var selectAgendas =  document.getElementById('selectAgendas');
+    var contenidoAgenda =  document.getElementById('contenidoAgenda');
 
     /**
      *   Cargamos todas las agendas que tenemos almacenadas en el localStorage
@@ -7,13 +10,22 @@ window.onload = function(){
      */
     function cargarOptionsSelect() {
         //limpiamos childnodes para que el select no duplique
-        borrarChildNodes(document.getElementById('selectAgendas'));
-        JSON.parse(localStorage.getItem("agendas")).forEach(function (elemento_agenda, indice) {
-            let option = document.createElement('option')
-            option.text = elemento_agenda;
-            option.value = indice;
-            document.getElementById('selectAgendas').appendChild(option)
-        })
+        borrarChildNodes(selectAgendas);
+        let agendas = JSON.parse(localStorage.getItem("agendas"));
+
+        if(agendas != null){
+            agendas.forEach(function (elemento_agenda, indice) {
+                if(elemento_agenda != null && elemento_agenda != undefined && elemento_agenda != "") {
+                    let option = document.createElement('option')
+                    option.text = elemento_agenda;
+                    option.value = indice;
+                    selectAgendas.appendChild(option)
+                } else {
+                    localStorage.removeItem(indice);
+                }
+            })
+        }
+
     }
 
     /**
@@ -26,21 +38,30 @@ window.onload = function(){
         }
     }
 
+    //Cargamos options
     cargarOptionsSelect();
+    //Forzamos que quede marcado el primer elemento
+    selectAgendas.value = 0;
+
 
     //Modificamos el contenido del textarea cuando cambiamos el option en el select
-    document.getElementById('selectAgendas').addEventListener('change', function(option){
-        let opcionSeleccionada = option.srcElement.value;
-        let contenidoLocalStorage = JSON.parse(localStorage.getItem(opcionSeleccionada));
-        document.getElementById('contenidoAgenda').value  = contenidoLocalStorage != null ? contenidoLocalStorage : "";
-    });
+    ["change","select"].forEach(action => {
+        selectAgendas.addEventListener(action, function(option){
+            let opcionSeleccionada = option.srcElement.value;
+            let contenidoLocalStorage = JSON.parse(localStorage.getItem(opcionSeleccionada));
+            contenidoAgenda.value  = contenidoLocalStorage != null ? contenidoLocalStorage : "";
+        });
+    })
 
-    document.forms['form-add-agenda'].addEventListener('submit', function(form){
+
+    formulario.addEventListener('submit', function(form){
         form.preventDefault();
         let value = form.srcElement.querySelectorAll('#inputAgenda')[0].value;
         agendas = JSON.parse(localStorage.getItem('agendas')) ;
         if(agendas != null){
-            agendas.push(value);
+            if(!agendas.includes(value)){
+                agendas.push(value);
+            }
         } else {
             agendas = [];
             agendas.push(value);
@@ -54,4 +75,32 @@ window.onload = function(){
         localStorage.setItem(valueAgenda,JSON.stringify(contenido.srcElement.value))
     })
 
+    function borrarContenidoAgenda(){
+        localStorage.removeItem(selectAgendas.value);
+        contenidoAgenda.value = localStorage.getItem(selectAgendas.value) == null ? "" : "Has borrado mal inutil";
+    }
+
+    document.getElementById("limpiarAgenda").addEventListener("click", function(){
+        borrarContenidoAgenda();
+    });
+
+    document.getElementById("borrarAgenda").addEventListener("click", function(){
+        let option = JSON.parse(localStorage.getItem("agendas"))[selectAgendas.value];
+        let agenda = [];
+        if (confirm("Estás seguro de que quieres borrar la agenda " + option + " ?")) {
+            agenda = JSON.parse(localStorage.getItem("agendas"));
+            if(agenda.includes(option)){
+                console.log(agenda[agenda.indexOf(option)]);
+                delete agenda[agenda.indexOf(option)]
+            }
+
+            localStorage.setItem("agendas", JSON.stringify(agenda))
+            cargarOptionsSelect();
+            borrarContenidoAgenda();
+
+
+        } else {
+
+        }
+    })
 }
