da76b4cc8a03b9ff973604b3959b0d3908522d6a
Adrián <adriangalende@gmail.com>
2019-03-02 10:45:28 +0100

    1 . [feature] al cambiar el textarea se guarda en localStorage 2 . [fix] al añadir una agenda se actualiza el desplegable

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 window.onload = function(){
     let agendas = []
 
-    JSON.parse(localStorage.getItem("agendas")).forEach( function(elemento_agenda, indice){
-        let option = document.createElement('option')
-        option.text = elemento_agenda;
-        option.value = indice;
-        document.getElementById('selectAgendas').appendChild(option)
-    })
+    /**
+     *   Cargamos todas las agendas que tenemos almacenadas en el localStorage
+     *
+     */
+    function cargarOptionsSelect() {
+        //limpiamos childnodes para que el select no duplique
+        borrarChildNodes(document.getElementById('selectAgendas'));
+        JSON.parse(localStorage.getItem("agendas")).forEach(function (elemento_agenda, indice) {
+            let option = document.createElement('option')
+            option.text = elemento_agenda;
+            option.value = indice;
+            document.getElementById('selectAgendas').appendChild(option)
+        })
+    }
+
+    /**
+     * Borra todos los nodos hijo de un nodo.
+     * @param node
+     */
+    function borrarChildNodes(node){
+        while(node.hasChildNodes()){
+            node.removeChild(node.lastChild);
+        }
+    }
+
+    cargarOptionsSelect();
+
+    //Modificamos el contenido del textarea cuando cambiamos el option en el select
+    document.getElementById('selectAgendas').addEventListener('change', function(option){
+        let opcionSeleccionada = option.srcElement.value;
+        let contenidoLocalStorage = JSON.parse(localStorage.getItem(opcionSeleccionada));
+        document.getElementById('contenidoAgenda').value  = contenidoLocalStorage != null ? contenidoLocalStorage : "";
+    });
 
     document.forms['form-add-agenda'].addEventListener('submit', function(form){
         form.preventDefault();
@@ -19,5 +46,12 @@ window.onload = function(){
             agendas.push(value);
         }
         localStorage.setItem("agendas", JSON.stringify(agendas));
+        cargarOptionsSelect();
     })
+
+    document.getElementById('contenidoAgenda').addEventListener('change', function(contenido){
+        let valueAgenda = document.getElementById('selectAgendas').value;
+        localStorage.setItem(valueAgenda,JSON.stringify(contenido.srcElement.value))
+    })
+
 }
